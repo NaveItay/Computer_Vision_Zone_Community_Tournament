4c59a5049ff72e67379f2c0ac8b2c1d23103d5db
Nave Itay <59213562+NaveItay@users.noreply.github.com>
2021-07-31 03:28:48 +0300

    Add files via upload

--- Main.py
+++ Main.py
@@ -3,25 +3,26 @@ from tools.car_counter_and_tracker import CarCountAndTrack
 import cv2
 
 o_detection = ObjectDetection()
-o_detection.initialize_model()
 cars_amount = CarCountAndTrack()
+
+# Initialize Model
+o_detection.initialize_model()
 
 video_path = "Video/DRONE-SURVEILLANCE-CONTEST-VIDEO.mp4"
 
-# Video
 cap = cv2.VideoCapture(video_path)
 
 while cap.isOpened():
     
     _, current_frame = cap.read()
 
-    # Detect cars
+    # Cars Detection
     classes, scores, detection_boxes = o_detection.detect(current_frame)
 
     # Count and track cars that are only in the region of interest!
     objects_bbs_ids = cars_amount.region_of_interest(current_frame, detection_boxes)
 
-    # Draw
+    # Draw result
     current_frame = o_detection.draw_objects(current_frame, classes, scores, objects_bbs_ids)
 
     cv2.imshow("Image", current_frame)
